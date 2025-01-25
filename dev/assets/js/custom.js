0ba152f95339cec506b2ad8d8b416456b71e4f73
OctoPrima <98483991+OctoPrima@users.noreply.github.com>
2025-01-25 19:22:00 +0200

    long page

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,31 @@
+const favoriteSwiper = new Swiper('.favorite-swiper', {
+    speed: 700,
+    slidesPerView: 2,
+    loop: false,
+    spaceBetween: 30,
+    grid: {
+      rows: 2,
+    },
   
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+const personalizedCoffeeSwiper = new Swiper('.personalized-coffee-swiper', {
+  loop: false,
+  speed: 700,
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+document.querySelector('.tabs') ? installTabs() : null;
